01cd0e32145b7c09b3bcc84453f7ab571545d783
zzzhyuan98 <82427810+zzzhyuan98@users.noreply.github.com>
2023-06-18 20:44:48 +0800

    add about

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+const about = ({ data }) => (
+  <React.Fragment>
+    <h2>{data.site.siteMetadata.title}</h2>
+    <div>{data.site.siteMetadata.description}</div>
+    <Link to="/">Back to home</Link>
+  </React.Fragment>
+)
+
+export default about
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        author
+        description
+        title
+      }
+    }
+  }
+`
